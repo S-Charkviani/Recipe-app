31f222be0a5c64c66632c24feea20fe08b45faa8
Salome Charkviani <salo.charkviani@gmail.com>
2022-12-09 20:30:13 +0400

    styling code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { FormsModule } from '@angular/forms';
 import { HighlighterDirective } from './highlighter.directive';
+import { DropdownDirective } from './dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { HighlighterDirective } from './highlighter.directive';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    HighlighterDirective
+    HighlighterDirective,
+    DropdownDirective
   ],
   imports: [
     FormsModule,
