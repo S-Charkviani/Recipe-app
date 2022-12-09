eddeab8c841a11c4271d5648be6bde0e38e79398
Salome Charkviani <salo.charkviani@gmail.com>
2022-12-09 15:00:30 +0400

    open details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,17 +1,23 @@
-import { outputAst } from '@angular/compiler';
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent implements OnInit{
-
-  recipes: Recipe[]=[
-    new Recipe('Meatballs', "Swedish meatballs with soy souce", "https://therecipecritic.com/wp-content/uploads/2016/08/swedishmeatballs2-3-500x500.jpg")
+export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected= new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe(
+      'Meatballs',
+      'Swedish meatballs with soy souce',
+      'https://therecipecritic.com/wp-content/uploads/2016/08/swedishmeatballs2-3-500x500.jpg'
+    ),
   ];
-  constructor(){}
-  ngOnInit(){}
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 
+  constructor() {}
+  ngOnInit() {}
 }
